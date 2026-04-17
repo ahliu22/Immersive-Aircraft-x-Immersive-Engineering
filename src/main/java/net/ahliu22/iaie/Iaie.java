b833509787337fabccf64ffeaa08134315bbba78
ahliu22 <liu2935944125@163.com>
2026-04-17 22:15:39 +0800

    Add files via upload

---
+++ src/main/java/net/ahliu22/iaie/Iaie.java
@@ -0,0 +1,14 @@
+package net.ahliu22.iaie;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Iaie.MODID)
+public class Iaie {
+    public final static String MODID = "iaie";
+
+    public Iaie() {
+
+    }
+
+    
+}
